4eebc3dba5b7f113e687f6993f13fb1ca92dd0dd
Cam Keeley <camtheanaconda@gmail.com>
2020-12-03 20:49:26 -0800

    Added some storyboard stuff

---
+++ TSP/MapViewController.swift
@@ -0,0 +1,47 @@
+//
+//  MapViewController.swift
+//  TSP
+//
+//  Created by William Keeley on 12/3/20.
+//  Copyright © 2020 CamKeeleyApps. All rights reserved.
+//
+//
+//  SecondViewController.swift
+//  databasePractice
+//
+//  Created by William Keeley on 12/1/20.
+//  Copyright © 2020 CamKeeleyApps. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    
+    @IBOutlet var label: UILabel!
+    
+    @IBOutlet var Description: UILabel!
+    
+    @IBOutlet var imageView: UIImageView!
+    
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+      
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
